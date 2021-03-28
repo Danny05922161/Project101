df885c47a3d8c50ebf9be40dcf335b6c518f275c
danny89019 <danny89019@gmail.com>
2021-03-28 16:51:42 +0800

    UI implement

--- src/main/java/org/project/App.java
+++ src/main/java/org/project/App.java
@@ -5,6 +5,7 @@ import org.project.DTO.BookInfoDTO;
 import org.project.Factory.RepoFactory;
 import org.project.Repo.Repository;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.util.List;
 
@@ -14,11 +15,12 @@ import java.util.List;
  */
 public class App {
 
-    private static String filename = "Rank_Chart.csv";
     private static String url = "https://data.tainan.gov.tw/dataset/f778b57a-d4b5-4f50-a386-d65e88b6204e/resource/55ea3647-882a-4183-aef5-a4153421a9e5/download/7.csv";
 
     public static void main( String[] args ) {
         List<BookInfoDTO> bookInfoDTOS = null;
+        JFrame frame = new JFrame("Project");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Download
         try {
             DownloadCSV downloadCSV = new DownloadCSV();
---
+++ src/main/java/org/project/ProjectFrame.java
@@ -0,0 +1,14 @@
+package org.project;
+
+import javax.swing.*;
+
+public class ProjectFrame {
+    private JTextField textField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField2;
+    private JTextField textField5;
+    private JButton 匯入資料Button;
+    private JButton 查詢Button;
+    private JTable table1;
+}
