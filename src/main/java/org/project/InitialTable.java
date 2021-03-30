1d03c7325182d513d3726d9598dd2eabbe477872
danny89019 <danny89019@gmail.com>
2021-03-30 21:10:52 +0800

    Add drop condition for initial

--- src/main/java/org/project/InitialTable.java
+++ src/main/java/org/project/InitialTable.java
@@ -10,20 +10,26 @@ public class InitialTable {
         Connection conn = null;
         Statement stmt = null;
         try {
+            StringBuilder stringBuilder = new StringBuilder();
             if(dbType.equals(DatabaseLookUp.DatabaseType.SQL_SERVER)){
                 conn = ConnectionUtil.getMSSQLConnection();
+                stringBuilder.append("IF EXISTS (SELECT * " +
+                        "FROM sys.tables " +
+                        "WHERE name = 'book_info' AND type = 'U') " +
+                        "DROP TABLE book_info;");
             }else {
                 conn = ConnectionUtil.getMSSQLConnection();
+                stringBuilder.append("DROP TABLE IF EXISTS 'book_info';");
             }
+            stringBuilder.append("CREATE TABLE book_info (" +
+                    "rank NUMERIC(10,0)," +
+                    "book_name VARCHAR(100)," +
+                    "author VARCHAR(100)," +
+                    "publisher VARCHAR(100)," +
+                    "book_counts NUMERIC(10,0))");
+
             stmt = conn.createStatement();
-            String sql = "IF EXISTS (SELECT * FROM sys.tables WHERE name = 'book_info' AND type = 'U') DROP TABLE book_info;" +
-                    "CREATE TABLE book_info " +
-                    "(rank NUMERIC(10,0), " +
-                    " book_name VARCHAR(100), " +
-                    " author VARCHAR(100), " +
-                    " publisher VARCHAR(100), " +
-                    " book_counts NUMERIC(10,0) )";
-            stmt.executeUpdate(sql);
+            stmt.executeUpdate(stringBuilder.toString());
             System.out.println("Created table in given database...");
         } catch (SQLException se) {
             System.out.println("Create Table Failed!");
